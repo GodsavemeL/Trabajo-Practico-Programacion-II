9c074fb9e83b46c394cf16da7c75d8681ae5aca3
Alejandro <alejandroeloso8@gmail.com>
2026-04-29 08:25:09 -0300

    Primer Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print("¡Bienvenido al sistema profesional!")
+
+def saludar_usuario():
+    nombre = input("¿Cuál es tu nombre de programador?: ")
+    print(f"Es un placer conocerte, {nombre}. Tu entorno está perfectamente configurado.")
+
+saludar_usuario()
+
+
